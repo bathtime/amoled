a6e077fde0b2df4f8ee60d9d95b78185b874012b
bathtime <andresmail@protonmail.com>
2022-06-25 16:52:50 -0400

    Updated project.

--- scripts/copy-to-projectfolder.sh
+++ scripts/copy-to-projectfolder.sh
@@ -6,6 +6,7 @@ path="/home/user/amoled-project"
 cd ~
 
 cp -r scripts.bak/* scripts
+exit
 
 #files=(scripts .local/share/plasma/desktoptheme/amoled .local/share/aurorae/themes/amoled .config/Kvantum/amoled .local/share/plasma/plasmoids .local/share/color-schemes .local/share/icons/Simply-Cyan-Circles .local/share/fonts .config/gtk-3.0 .config/gtk-4.0)
 
