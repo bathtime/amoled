3d2c6b528be86f7367e7287188f0e35af866af98
bathtime <andresmail@protonmail.com>
2022-07-17 16:47:12 -0400

    Updated project.

---
+++ .local/share/plasma/plasmoids/com.marcinorlowski.htmlclock/contents/js/meta.js
@@ -0,0 +1,14 @@
+// This file is auto-generated. DO NOT EDIT BY HAND
+// Generated: 2021-12-12T14:55:41+01:00
+
+// https://doc.qt.io/qt-5/qtqml-javascript-resources.html
+.pragma library
+
+const version="1.6.2"
+const title="HTML Clock"
+const url="https://github.com/MarcinOrlowski/html-clock-plasmoid"
+const authorName="Marcin Orlowski"
+const authorUrl="http://marcinorlowski.com/"
+const updateCheckerUrl="https://raw.githubusercontent.com/MarcinOrlowski/html-clock-plasmoid/master/src/metadata.desktop"
+const firstReleaseYear=2020
+
