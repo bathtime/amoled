b8cab60a1cef0fcc609183b917c93fc916985719
bathtime <andresmail@protonmail.com>
2022-08-01 12:15:10 -0400

    Updated project

---
+++ .icons/Amoled/cursors/link.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+cd ~/.icons/Amoled/cursors
+
+ln -fs crosshair cross
+ln -fs crosshair plus
+ln -fs crosshair tcross
+
+ln -fs crossed_circle circle
+ln -fs crossed_circle not-allowed
+ln -fs crossed_circle pirate
+
+
+ln -fs right_ptr arrow
+ln -fs right_ptr draft_large
+ln -fs right_ptr draft_small
+
+ln -fs left_ptr top_left_arrow
+
+ln -fs hand hand1
+ln -fs hand hand2
+ln -fs hand pointer
+ln -fs hand pointing_hand
+
+
+ln -fs left_side left_tee
+
+ln -fs left_ptr ul_angle
+ln -fs left_ptr ur_angle
+
+ln -fs closedhand dnd-move
+ln -fs closedhand dnd-none
+ln -fs closedhand grabbing
+ln -fs closedhand move
+
+ln -fs progress half-busy
+ln -fs progress left_ptr_watch 
+
+ln -fs size_hor e-resize
+ln -fs size_hor h_double_arrow
+ln -fs size_hor w-resize
+
+ln -fs size_ver v_double_arrow 
+ln -fs size_ver sb_v_double_arrow
+ln -fs size_ver s_resize
+ln -fs size_ver n_resize
+
+ln -fs size_fdiag nwse-resize
+ln -fs size_bdiag nesw-resize
+
+ln -fs fleur size_all
+
+ln -fs help whats_this 
+ln -fs help question_arrow
+ln -fs help left_ptr_help
+
+ln -fs openhand grab 
+
+ln -fs wait watch
+
